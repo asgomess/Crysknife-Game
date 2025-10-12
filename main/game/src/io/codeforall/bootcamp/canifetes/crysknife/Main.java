10e9f26249511da5bf6c811bb4d396fbd52010d8
rodriiabj <rodriabj1@gmail.com>
2025-10-12 14:39:27 +0100

    update

--- main/game/src/io/codeforall/bootcamp/canifetes/crysknife/Main.java
+++ main/game/src/io/codeforall/bootcamp/canifetes/crysknife/Main.java
@@ -1,6 +1,9 @@
 package io.codeforall.bootcamp.canifetes.crysknife;
 
-import 
+import org.academiadecodigo.simplegraphics.graphics.Rectangle;
 
 public class Main {
+    public static void main(String[] args) {
+        
+    }
 }
